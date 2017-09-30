05b3ca8e1a66b4471a4f6a3167d09de6581a29e2
annclair <anneclaire.sebirou@gmail.com>
2017-09-30 13:40:54 +0200

    disconnect

--- public/js/components/common/navBar.js
+++ public/js/components/common/navBar.js
@@ -1,6 +1,25 @@
 ((app) => {
     'use strict'
     app.component('navBar', {
-        templateUrl: 'js/components/common/navBar.html'
+        templateUrl: 'js/components/common/navBar.html',
+        controller: ['$state', 'usersService', function ($state, usersService) {
+
+            angular.extend(this, {
+                $onInit() {
+                    this.getUser();
+                },
+                getUser() {
+                    usersService.getCurrent().then((user) => {
+                        this.user = user
+                    })
+                },
+                disconnect() {
+                    usersService.disconnect().then((user) => {
+                        this.user = null
+                        $state.reload()
+                    })
+                }
+            })
+        }]
     })
 })(require('angular').module('app.common'))
